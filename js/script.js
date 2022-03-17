5dab6d747ab5839747c7d08005d27b6e24fb7d9b
Md. Ashikur Rahman <48981854+atvhm@users.noreply.github.com>
2022-03-18 05:27:22 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ================== typing animation =================== */
+var typed = new Typed(".typing",{
+    strings:[" ","Undergraduate Student","Front-End Developer","Data Analyst","UI/UX Designer", "Researcher"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true
+})
