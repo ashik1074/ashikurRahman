4ef8abaafd243771365f714deb1cc0d139e014c2
Md. Ashikur Rahman <48981854+ashik1074@users.noreply.github.com>
2022-07-30 21:53:39 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ================== typing animation =================== */
 var typed = new Typed(".typing",{
-    strings:[" ","Undergraduate Student","Front-End Developer","Data Analyst","UI/UX Designer", "Researcher"],
+    strings:[" ","CSE Graduate","Software Developer","Data Analyst","UI/UX Designer", "Researcher"],
     typeSpeed:100,
     BackSpeed:60,
     loop: true
